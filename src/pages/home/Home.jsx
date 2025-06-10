b303318e7ed8b9073d81cbb7639136b40a63605c
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-06-10 09:33:48 +0700

    feat: add layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,6 @@
 
 export default function HomePage(){
     return(
-        <h1>Home page</h1>
+        <h1 className="text-light">Home page</h1>
     )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
 //router
 
+import BlogLayout from "../layouts/BlogLayout";
+import HomeLayout from "../layouts/HomeLayout";
+import BlogPage from "../pages/blog/Blog";
 import HomePage from "../pages/home/Home";
 
-
 export const routers = [
-    {
-        path : "/",
-        component: HomePage,
-        layout: null
-    }
-]
+  {
+    path: "/",
+    component: HomePage,
+    layout: HomeLayout,
+  },
+  {
+    path: "/blog",
+    component: BlogPage,
+    layout: BlogLayout,
+  },
+];
